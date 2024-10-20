af787b9747e66bd901e543bc69c16fdb707eddde
maryamMH100 <mar0iam100@gmail.com>
2024-10-20 09:05:01 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Maryam on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
